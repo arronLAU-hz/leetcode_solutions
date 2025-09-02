e5438580f97b233f1eaa0b3d7ac55eb786ca1827
Arron Lau <arronliu1993@gmail.com>
2025-09-02 15:52:54 +0800

    update

--- Twosum.py
+++ Twosum.py
@@ -1,1 +1,19 @@
-import
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}  # 值 -> 下标
+        for i, x in enumerate(nums):
+            need = target - x
+            if need in seen:
+                return [seen[need], i]
+            seen[x] = i
+
+        raise ValueError("No solution")
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2, 7, 11, 15], 9))   # 输出 [0, 1]
+    print(sol.twoSum([3, 2, 4], 6))        # 输出 [1, 2]
+    print(sol.twoSum([3, 3], 6))
