c412989601c54cd8eb29aa22aa1af4ffeb481450
Arron Lau <arronliu1993@gmail.com>
2025-09-04 17:20:47 +0800

    update

--- TwosumII.py
+++ TwosumII.py
@@ -6,7 +6,7 @@ class Solution:
         while l < r:
             s = numbers[l] + numbers[r]
             if s == target:
-                return [l + 1, r + 1]  # 转为1下标
+                return [l + 1, r + 1]
             elif s < target:
                 l += 1
             else:
@@ -15,7 +15,7 @@ class Solution:
 
 
 if __name__ == "__main__":
-    # 测试用例
+
     numbers = [2, 7, 11, 15]
     target = 9
     solution = Solution()
